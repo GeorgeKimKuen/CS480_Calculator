5f5bbba3d4ec3f0adde40d07ce62d7cb9dc044ff
Geotiger123 <82854175+Geotiger123@users.noreply.github.com>
2022-10-12 02:58:06 -0700

    Update Form1.cs
    - Added button functions to appear on the textbox

--- Calulator/Calulator/Form1.cs
+++ Calulator/Calulator/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +12,10 @@ namespace Calulator
 {
     public partial class Form1 : Form
     {
+        Double results = 0;
+        String operation;
+        bool enter_value = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,54 +26,124 @@ namespace Calulator
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button_Decimal(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+            txtDisplay.Text += num.Text;
+        }
+
+        private void button_Negative(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "-";
+        }
+
+        private void button_Equal(object sender, EventArgs e)
         {
 
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button_Plus(object sender, EventArgs e)
         {
+            txtDisplay.Text += "+";
+        }
+
+        private void button_Minus(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "-";
 
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button_Mult(object sender, EventArgs e)
         {
+            txtDisplay.Text += "*";
 
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void button_Div(object sender, EventArgs e)
         {
+            txtDisplay.Text += "/";
 
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button_Back(object sender, EventArgs e)
         {
+            if (txtDisplay.Text.Length > 0)
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+        }
+
+        private void button_Clear(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+        }
+
+        private void button_power(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "^";
+        }
+
+        private void button_Nat_Log(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "Ln(";
+        }
+
+        private void button_Log(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "Log(";
 
         }
 
-        private void button13_Click(object sender, EventArgs e)
+        private void button_Cos(object sender, EventArgs e)
         {
+            txtDisplay.Text += "cos(";
 
         }
 
-        private void button17_Click(object sender, EventArgs e)
+        private void button_Sin(object sender, EventArgs e)
         {
+            txtDisplay.Text += "sin(";
 
         }
 
-        private void button14_Click(object sender, EventArgs e)
+        private void button_Tan(object sender, EventArgs e)
         {
+            txtDisplay.Text += "tan(";
 
         }
 
-        private void button22_Click(object sender, EventArgs e)
+        private void button_Cot(object sender, EventArgs e)
         {
+            txtDisplay.Text += "cot(";
 
         }
 
-        private void button29_Click(object sender, EventArgs e)
+        private void button_Left_Curl(object sender, EventArgs e)
         {
+            txtDisplay.Text += "{";
 
+        }
+
+        private void button_Right_Curl(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "}";
+
+        }
+
+        private void button_Left_Para(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "(";
+
+        }
+
+        private void button_Right_Para(object sender, EventArgs e)
+        {
+            txtDisplay.Text += ")";
+
+        }
+
+        private void button_Num(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+            txtDisplay.Text += num.Text;
         }
     }
 }
